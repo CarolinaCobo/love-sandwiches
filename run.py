67ad800c3c7c6571e4bb39f672a50420b06b401a
Carolina <ccobo.dev@gmail.com>
2021-07-22 21:05:30 +0100

    Add user request loop to repeat the request for data until data provideed is valid

--- run.py
+++ run.py
@@ -19,15 +19,20 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
 
-
+        sales_data = data_str.split(",")
+    
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+        
+    return sales_data
 
 def validate_data(values):
     """
@@ -43,6 +48,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-get_sales_data()
+data = get_sales_data()
