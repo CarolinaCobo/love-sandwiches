ecf51e9976628a9f49d371c063911376d77aa4a3
Carolina <ccobo.dev@gmail.com>
2021-07-22 20:58:23 +0100

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -36,6 +36,7 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
